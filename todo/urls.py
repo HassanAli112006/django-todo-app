0533de5475f3eb5f75cb23bbb167a33c9e233250
Hassan Ali <ha526779@gmail.com>
2025-07-29 23:15:47 +0500

    Added Comments to explain and arrange code

--- todo/urls.py
+++ todo/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add Task Routing
     path('add_task/', views.add_task, name='add_task'),
+    # Complete Task Routing
     path('done/<int:pk>/', views.done, name='done'),
+    # Uncomplete Task Routing
     path('undone/<int:pk>/', views.undone, name='undone'),
+    # Add Task Routing
     path('edit/<int:pk>/', views.edit, name='edit'),
-    path('delete_task/<int:pk>', views.delete_task, name='delete_task'),
+    # Delete task Routing
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,24 +2,30 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
-# Create your views here.
+# Function to add a new task
 def add_task(request):
     new_task = request.POST.get('entered_task')
     Task.objects.create(task = new_task)
     return redirect('home_page')
 
+
+# Function to mark a task Done
 def done(request, pk):
     task = get_object_or_404(Task , pk = pk)
     task.is_completed = True
     task.save()
     return redirect('home_page')
 
+
+# Function to mark a task Undone
 def undone(request, pk):
     task = get_object_or_404(Task , pk = pk)
     task.is_completed = False
     task.save()
     return redirect('home_page')
 
+
+# Function to edit a task
 def edit(request, pk):
     task = get_object_or_404(Task, pk = pk)
     new_task = request.POST.get('updated_task')
@@ -34,6 +40,8 @@ def edit(request, pk):
         }
         return render(request, 'edit.html', context)
 
+
+# Function to delete tasks
 def delete_task(request, pk):
     task = get_object_or_404(Task, pk = pk)
     task.delete()
